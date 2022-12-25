cd964a92c0358e1d4a8ad507b804c5e26849220d
Jaakko Mäkilaurila <j.makilaurila@gmail.com>
2022-12-25 17:56:42 +0200

    Wrap Layout component around next Component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,25 @@
-const Layout = () => {
-  return <div>Layout</div>
+import { FC, ReactNode } from 'react'
+import Head from 'next/head'
+import Footer from './Footer'
+import Navbar from './Navbar'
+
+const Layout: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Webshop</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import Layout from 'components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Head>
-      <title>Webshop</title>
-    </Head>
-    <Component {...pageProps} />
-  </>
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
