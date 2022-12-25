b7ad895bc945c207d12bde8d695abf8ee7a117ac
Jaakko Mäkilaurila <j.makilaurila@gmail.com>
2022-12-25 12:17:49 +0200

    Add Sanity client to the frontend

---
+++ lib/client.ts
@@ -0,0 +1,15 @@
+import sanityClient from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+import { SanityImageSource } from '@sanity/image-url/lib/types/types'
+
+export const client = sanityClient({
+  projectId: 'omcgv00h',
+  dataset: 'production',
+  useCdn: true,
+  apiVersion: '2022-12-15',
+  token: process.env.NEXT_PUBLIC_READONLY_SANITY_TOKEN,
+})
+
+const builder = imageUrlBuilder(client)
+
+export const urlFor = (source: SanityImageSource) => builder.image(source)
