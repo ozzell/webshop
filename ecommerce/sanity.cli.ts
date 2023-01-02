caa32aae06650c20e0a343e4591cdb6f18f16ab1
Jaakko Mäkilaurila <j.makilaurila@gmail.com>
2023-01-02 14:06:02 +0200

    Remove sanity application from repo (separated to its own repo)

--- ecommerce/sanity.cli.ts
+++
@@ -1,9 +0,0 @@
-import { defineCliConfig } from 'sanity/cli'
-
-/* There may be problem reading the env variables here */
-export default defineCliConfig({
-  api: {
-    projectId: import.meta.env.SANITY_STUDIO_API_PROJECT_ID,
-    dataset:import.meta.env.SANITY_STUDIO_API_DATASET
-  }
-})
