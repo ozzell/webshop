f2ebf3c6d28da009356211170b086a2086be8247
Jaakko Mäkilaurila <j.makilaurila@gmail.com>
2023-01-03 20:33:26 +0200

    Add styled-components, remake HeroBanner styles using styled

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,11 +1,17 @@
 import { FC } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import styled from 'styled-components'
 import { BannerData } from 'types'
 import { urlFor } from 'pages/api/sanity-client'
 
+interface HeroBannerProps extends BannerData {
+  className?: string
+}
+
 // @ TODO image size issue
-const HeroBanner: FC<BannerData> = ({
+const HeroBannerComponent: FC<HeroBannerProps> = ({
+  className,
   _id,
   smallText,
   midText,
@@ -20,34 +26,122 @@ const HeroBanner: FC<BannerData> = ({
   }
 
   return (
-    <div className="hero-banner-container">
-      <div>
-        <p className="beats-solo">{smallText}</p>
+    <div className={className}>
+      <LeftSideContainer>
+        <p>{smallText}</p>
         <h3>{midText}</h3>
         <h4>{largeText1}</h4>
+        {/* @ TODO button inside a link accessibility issue */}
+        <Link href={`/product/${product}`}>
+          <button>{buttonText}</button>
+        </Link>
+      </LeftSideContainer>
+      <RightSideContainer>
         {image && (
-          <Image
+          <StyledImage
             width="450"
             height="450"
             src={urlFor(image).url()}
             alt="headphones"
-            className="hero-banner-image"
             priority
           />
         )}
-        <div>
-          {/* @ TODO button inside a link accessibility issue */}
-          <Link href={`/product/${product}`}>
-            <button>{buttonText}</button>
-          </Link>
-          <div className="desc">
-            <h5>Descriptions</h5>
-            <p>{desc}</p>
-          </div>
-        </div>
-      </div>
+        <StyledDesc>
+          <h5>Description</h5>
+          <p>{desc}</p>
+        </StyledDesc>
+      </RightSideContainer>
     </div>
   )
 }
+
+HeroBannerComponent.displayName = 'HeroBanner'
+
+const StyledImage = styled(Image)`
+  @media (max-width: 768px) {
+    width: 80%;
+    height: 80%;
+  }
+  @media (max-width: 425px) {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+const LeftSideContainer = styled.div`
+  justify-content: center;
+
+  @media (max-width: 768px) {
+    padding: 4.25rem 2.25rem;
+  }
+`
+
+const RightSideContainer = styled.div`
+  align-items: end;
+  padding-right: 6.25rem;
+
+  @media (max-width: 768px) {
+    padding: 0;
+    align-items: start;
+  }
+`
+
+const StyledDesc = styled.div`
+  line-height: 1.3;
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 768px) {
+    padding: 1rem 2.25rem 2.25rem;
+  }
+
+  h5 {
+    color: #324d67;
+    margin-bottom: 0.75rem;
+    font-weight: 700;
+    font-size: 1rem;
+  }
+
+  p {
+    color: #5f5f5f;
+  }
+`
+
+const HeroBanner = styled(HeroBannerComponent)`
+  display: flex;
+  justify-content: space-between;
+  background-color: #dcdcdc;
+  border-radius: 15px;
+  padding: 4.25rem 6.25rem;
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+
+  h3 {
+    font-size: 2.5rem;
+  }
+
+  h4 {
+    font-size: 3.5rem;
+  }
+
+  button {
+    background-color: #f02d34; // #000
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    padding: 0.5rem 1rem;
+    font-size: 1.5rem;
+    cursor: pointer;
+    margin-top: 1rem;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0;
+  }
+`
 
 export default HeroBanner
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    styledComponents: true
+  },
   images: {
     domains: ['cdn.sanity.io']
   },
